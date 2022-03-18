91cdca803e5984a5e9cd806a2870359b56e54316
unknown <william.nahirnei@hotmail.com>
2022-03-17 21:50:33 -0300

    Configure timezone conection

--- api/config/database.js
+++ api/config/database.js
@@ -5,7 +5,8 @@ const knex = require('knex')({
       host : process.env.DB_HOST,
       user : process.env.DB_USER,
       password : process.env.DB_PASSWORD,
-      database : process.env.DB_DATABASE
+      database : process.env.DB_DATABASE,
+      timezone: process.env.DB_TIMEZONE
     }
   })
 module.exports = knex
